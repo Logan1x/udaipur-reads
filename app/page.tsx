ba709c25928ceb5883c666c2b18b68cc4b391d93
logan1x <sharmakhushal78@gmail.com>
2024-05-20 17:17:15 +0530

    Initial website

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,165 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+
+interface Book {
+  bookName: string;
+  author: string;
+  avgRating: string;
+  dateAdded: Date;
+  goodReadsURL: string;
+  bookCover: string;
+}
 
 export default function Home() {
+  const [books, setBooks] = useState<Book[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const parseRSS = (data: string) => {
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(data, "text/xml");
+    return xml;
+  };
+
+  const logBookDetails = (items: HTMLCollectionOf<Element> | any) => {
+    const books = [];
+    for (let i = 0; i < items.length; i++) {
+      let bookName =
+        items[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+      let author =
+        items[i].getElementsByTagName("author_name")[0].childNodes[0].nodeValue;
+      let avgRating =
+        items[i].getElementsByTagName("average_rating")[0].childNodes[0]
+          .nodeValue; // Adjust this based on your RSS structure
+      let dateAdded = new Date(
+        items[i].getElementsByTagName(
+          "user_date_added"
+        )[0].childNodes[0].nodeValue
+      );
+
+      let goodReadsURL = items[i]
+        .getElementsByTagName("description")[0]
+        .childNodes[1].nodeValue.match(/href="([^"]*)/)[1];
+      goodReadsURL = goodReadsURL.split("?")[0];
+
+      let bookCover = items[i].getElementsByTagName("book_medium_image_url")[0]
+        .childNodes[0].nodeValue;
+
+      books.push({
+        bookName,
+        author,
+        avgRating,
+        dateAdded,
+        goodReadsURL,
+        bookCover,
+      });
+    }
+    return books;
+  };
+
+  const getData = async () => {
+    setIsLoading(true);
+    const res = await axios.get("/sync");
+    const parsedRSS = parseRSS(res.data);
+    const items = parsedRSS.getElementsByTagName("item");
+    const parsedBooks = logBookDetails(items);
+    setBooks(parsedBooks);
+    setIsLoading(false);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="grow flex flex-col items-center justify-between p-20 bg-slate-50 text-slate-800">
+      <div className="container text-center">
+        {isLoading ? (
+          <h1>
+            Loading <span className="animate-ping">...</span>
+          </h1>
+        ) : books.length === 0 ? (
+          <>
+            <h1 className="mt-2 text-3xl">
+              Welcome to Udaipur Book Club's Showpage.
+            </h1>
+            <p className="mb-2 text-md text-slate-600">
+              Click below to know our favorites.
+            </p>
+            <button
+              onClick={getData}
+              className="bg-slate-900 text-slate-50 px-4 py-2 mt-4 rounded shadow-md hover:bg-slate-800  transition-all duration-300"
+            >
+              Get Books
+            </button>
+          </>
+        ) : (
+          <>
+            <h1 className="mb-6 text-3xl">Past picks</h1>
+            <div className="flex justify-center flex-wrap gap-6 text-left">
+              {books.map((book) => (
+                <Link
+                  key={book.bookName}
+                  className="border w-48 p-2 flex flex-col gap-1 rounded shadow-sm hover:bg-slate-100 hover:shadow-md transform hover:scale-105 transition-all duration-300"
+                  href={book.goodReadsURL}
+                  target="_blank"
+                >
+                  <img
+                    className="justify-center w-48 h-72 object-cover rounded-sm"
+                    src={book.bookCover}
+                    alt={book.bookName}
+                  />
+                  <h2
+                    className={
+                      book.bookName.length > 20
+                        ? `font-semibold truncate ...`
+                        : "font-semibold"
+                    }
+                  >
+                    {book.bookName}
+                  </h2>
+                  <p>{book.author}</p>
+                  <div className="text-xs text-slate-500 flex items-center">
+                    <svg
+                      dataSlot="icon"
+                      fill="none"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      className="w-4 h-4 inline-block mr-1"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                      />
+                    </svg>
+                    <span>{book.avgRating}</span>
+                  </div>
+                  <p className="text-xs text-slate-500">
+                    <span>
+                      <svg
+                        dataSlot="icon"
+                        fill="none"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                        className="w-4 h-4 inline-block mr-1"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                        />
+                      </svg>
+                    </span>
+                    {book.dateAdded.toDateString()}
+                  </p>
+                </Link>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </main>
   );
---
+++ app/sync/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+// To handle a GET request to /api
+export async function GET() {
+  try {
+    const rssText = await axios.get(
+      "https://www.goodreads.com/review/list_rss/87975662?key=wGWAksug_4259__QFBUCwZBbK9dwUzAkVPkeDAOZPEKXy6mn&shelf=to-read"
+    );
+
+    return NextResponse.json(rssText.data, { status: 200 });
+  } catch (e) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
+}
